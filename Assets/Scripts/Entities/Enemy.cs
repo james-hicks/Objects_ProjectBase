cd782cf83bec41980c4715918a911924cc55353c
William-Fleming <wfleming2016@gmail.com>
2025-08-07 15:53:23 -0700

    Added fire trail to player and made visualization to homing bullet to be different

--- Assets/Scripts/Entities/Enemy.cs
+++ Assets/Scripts/Entities/Enemy.cs
@@ -83,7 +83,7 @@ public class Enemy : PlayableObject
 
     public override void Die()
     {
-       // spawnParticles();
+       spawnParticles();
         GameManager.GetInstance().scoreManager.IncrementScore(ScoreValue);
         GameManager.GetInstance().NotifyDeath(this);
         Destroy(gameObject);
@@ -97,6 +97,7 @@ public class Enemy : PlayableObject
     private void spawnParticles()
     {
         explosionInstance = Instantiate(onDeathExplosion, transform.position, quaternion.identity);
+        Destroy(explosionInstance.gameObject, 3f);
     }
 
 
